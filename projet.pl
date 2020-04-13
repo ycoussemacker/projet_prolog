a25cc072c146e5a08f4935a4051f274e276b7d65
NinsF <48688866+NinsF@users.noreply.github.com>
2020-04-13 10:56:01 +0200

    vainqueur

--- projet.pl
+++ projet.pl
@@ -26,7 +26,7 @@
 % les joueurs
 joueurs([utilisateur, ordi]).
 
-% liste des 16 perso
+% liste des 16 personnages
 personnage(girafe).
 personnage(lion).
 personnage(morse).
@@ -44,7 +44,7 @@ personnage(poisson).
 personnage(souris).
 personnage(serpent).
 
-% liste des 3policiers
+% liste des 3 policiers
 personnage(policier1).
 personnage(policier2).
 personnage(policier3).
@@ -59,7 +59,7 @@ viseur(2,3).
 viseur(3,2).
 viseur(3,3).
 
-%  Liste armes
+%  Liste des armes
 arme(pistolet).
 arme(couteau).
 arme(fusil).
@@ -67,7 +67,7 @@ arme(fusil).
 % -------- DEBUT DE LA PARTIE ON INITIALISE TOUTES LES DONNEES ---------
 
 % -- Prédicat tuile (ajout à la bdd de toutes les tuiles possibles initialisées avec des listes de perso vides)
-% tous les personnages sont morts au début. On va les ressusiter (=déplacer dans la liste des vivant) en attribuant à chaque case un de ces personnages.
+% tous les personnages sont morts au début. On va les ressusciter (=déplacer dans la liste des vivants) en attribuant à chaque case un de ces personnages.
 :-asserta(morts([girafe,lion, morse, crocodile, renard, lapin, loup, chat, chien, canard,oie, elephant, loutre, poisson, souris, serpent])).
 :-asserta(inutiles([girafe,lion, morse, crocodile, renard, lapin, loup, chat, chien, canard,oie, elephant, loutre, poisson, souris, serpent])).
 :-asserta(vivants([])).
@@ -79,8 +79,8 @@ arme(fusil).
 :- asserta(tuile(1,3,[])).
 :- asserta(tuile(1,4,[])).
 :- asserta(tuile(2,1,[])).
-:- asserta(tuile(2,3,[])).
 :- asserta(tuile(2,2,[])).
+:- asserta(tuile(2,3,[])).
 :- asserta(tuile(2,4,[])).
 :- asserta(tuile(3,1,[])).
 :- asserta(tuile(3,2,[])).
@@ -92,16 +92,16 @@ arme(fusil).
 :- asserta(tuile(4,4,[])).
 
 debutPartie():- vivants(L),             % On récupère la liste des vivants
-                longueur(N,L), N < 1,   % On démare la partie seulement si la liste des vivants est vide
+                longueur(N,L), N < 1,   % On démarre la partie seulement si la liste des vivants est vide
                 attribution(), 
                 donneTueur(),
                 donneCibles(), 
                 joueurs(ListeJoueurs), random_member(J,ListeJoueurs), assert(tour(J)), %On choisi au hasard qui commence
                 !.
 
-attribution():- retract(tuile(Ligne, Colonne, LP)),         % On supprime la tuile et on récupère chaque Ligne et colonne (début de la boucle)
+attribution():- retract(tuile(Ligne, Colonne, LP)),         % On supprime la tuile et on récupère chaque ligne et colonne (début de la boucle)
                 retract(morts(ListeMorts)),                 % On récupère la liste des personnages à placer (en la supprimant)
-                random_member(Perso, ListeMorts),           % on choisi au hasard un Personnage dans la liste des perso à placer                
+                random_member(Perso, ListeMorts),           % On choisi au hasard un personnage dans la liste des perso à placer                
                 assert(tuile(Ligne,Colonne, [Perso|LP])),   % On ajoute le personnage à la tuile correspondante 
                 retract(vivants(ListeVivants)),
                 assert(vivants([Perso|ListeVivants])),      % On ajoute la liste des vivants avec le personnage ajouté
@@ -109,24 +109,24 @@ attribution():- retract(tuile(Ligne, Colonne, LP)),         % On supprime la tui
                 assert(morts(NewList)),                     % On ajoute la nouvelle liste des perso à placer
                 longueur(N, NewList), N < 1.                % On termine le programme si il n'y a plus de perso à placer. (Fin de la boucle)
 
-donneTueur():-  retract(inutiles(L1)),              %recupère la liste des perso inutilisés
+donneTueur():-  retract(inutiles(L1)),              % Recupère la liste des perso inutilisés
                 random_member(Perso1, L1),          % Choisi au hasard un personnage dans cette liste
-                assert(tueur(utilisateur,Perso1)),  % selectionne ce personnage comme tueur pour l'utilisateur
-                supprimer(Perso1, L1, L2),          % supprime ce personnage de la liste des inutiles
-                random_member(Perso2, L2),          % choisi un deuxième utilisateur
-                assert(tueur(ordi,Perso2)),          % selectionne ce perso comme tueur de l'ordi
-                supprimer(Perso2, L2, L3),          % supprime ce perso de la liste des inutiles
-                assert(inutiles(L3)).                % actualise la liste des inutiles.
+                assert(tueur(utilisateur,Perso1)),  % Sélectionne ce personnage comme tueur pour l'utilisateur
+                supprimer(Perso1, L1, L2),          % Supprime ce personnage de la liste des inutiles
+                random_member(Perso2, L2),          % Choisi un deuxième personnage
+                assert(tueur(ordi,Perso2)),         % Selectionne ce perso comme tueur de l'ordi
+                supprimer(Perso2, L2, L3),          % Supprime ce perso de la liste des inutiles
+                assert(inutiles(L3)).               % Actualise la liste des inutiles.
 
 donneCibles():- donneCibles(utilisateur), donneCibles(ordi).
 donneCibles(J):-
                 retract(inutiles(L1)),              % On recupère la liste des inutilisés
                 random_member(Perso1, L1),          % On choisi au hasard un perso parmi la liste
-                supprimer(Perso1, L1, L2),          % on supprime ce personnage de la liste des inutiles
+                supprimer(Perso1, L1, L2),          % On supprime ce personnage de la liste des inutiles
                 random_member(Perso2, L2),          % On choisi au hasard un 2ème perso parmi la nouvelle liste
-                supprimer(Perso2, L2, L3),          % supprime ce personnage de la liste des inutiles
-                random_member(Perso3, L3),           % On choisi au hasard un 3ème perso parmi la nouvelle liste
-                supprimer(Perso3, L3, L4),          % supprime ce personnage de la liste des inutiles 
+                supprimer(Perso2, L2, L3),          % Suppression de ce personnage de la liste des inutiles
+                random_member(Perso3, L3),          % On choisi au hasard un 3ème perso parmi la nouvelle liste
+                supprimer(Perso3, L3, L4),          % Suppression de ce personnage de la liste des inutiles 
                 assert(inutiles(L4)),               % On actualise la liste des inutiles
                 retract(cibles(J,L)), 
                 assert(cibles(J,[Perso3|[Perso2|[Perso1|L]]])).
@@ -139,6 +139,11 @@ donneCibles(J):-
 nouveauTour():-tour(utilisateur), retract(tour(utilisateur)), assert(tour(ordi)).
 nouveauTour():-tour(ordi), retract(tour(ordi)), assert(tour(utilisateur)).
 
+% Fin du jeu et attribution du titre vainqueur en fonction des points
+vainqueur(null).
+vainqueur() :- vainqueur(utilisateur), pointsUtilisateur(p1), pointsOrdi(p2), p1 > p2, write ('Félicitations vous avez gagné!').
+vainqueur() :- vainqueur(ordi), pointsUtilisateur(p1), pointsOrdi(p2), p1 < p2, write ('Vous avez perdu !').
+
 
 % ----------------------- ACTIONS DES JOUEURS ---------------------------
 
@@ -147,7 +152,7 @@ nouveauTour():-tour(ordi), retract(tour(ordi)), assert(tour(utilisateur)).
 deplacer(Perso, Ligne, Colonne) :- personnage(Perso), supprimer(Perso), ajouter(Perso, Ligne, Colonne), !.
 % Ajoute le personnage à la tuile renseignée
 ajouter(Perso, Ligne, Colonne) :-  retract(tuile(Ligne,Colonne, ListePerso)), asserta(tuile(Ligne,Colonne,[Perso|ListePerso])).
-% Cherche parmis toutes les tuiles qu'il possède celle qui contient le personnage et supprime le personnage de cette dernière.
+% Cherche parmi toutes les tuiles qu'il possède celle qui contient le personnage et le supprime de cette dernière.
 supprimer(Perso) :- tuile(L,C,ListePerso), dans(Perso,ListePerso), supprimer(Perso,ListePerso, NewList), retract(tuile(L,C,_)), asserta(tuile(L,C,NewList)).% On récupère la liste des perso à partir de ligne et ccolonne pour en supprimer le Perso
 
 % ----- PEUT TUER ------ 
@@ -167,7 +172,7 @@ peutTuer(P1,P2,Xt,Yt):- personnage(P1), tuile(X,Yt,L),
                         personnage(P2), P1\=P2, tuile(Xt,Yt,Li), dans(P2,Li).    % On cherche une tuile dans la même colonne où P2 se trouve. Si on trouve une tuile ==> P1 peut tuer P2
 peutTuer(P1,P2,Xt,Yt):- personnage(P1), tuile(Xt,Y,L), 
                         dans(P1,L), longueur(N,L), N==1 , viseur(Xt,Y),          % On vérifie que P1 est seul dans sa case
-                        personnage(P2), P1\=P2, tuile(Xt,Yt,Li), dans(P2,Li),!.    % On cherche une tuile dans la même ligne où P2 se trouve. Si on trouve une tuile ==> P1 peut tuer P2
+                        personnage(P2), P1\=P2, tuile(Xt,Yt,Li), dans(P2,Li),!.  % On cherche une tuile dans la même ligne où P2 se trouve. Si on trouve une tuile ==> P1 peut tuer P2
 
 
 % ----- TUE ------
@@ -179,12 +184,12 @@ tue(P1,P2):-    peutTuer(P1,P2,X,Y),
 
 donnePoints(P2):-   tour(J), points(J,N), 
                     cibles(J,LC), dans(P2,LC), NewN is N + 1,               % Si il a éliminé une de ses cibles
-                    retract(points(J,N)), assert(points(J,NewN)),           % on actualise les points
-                    retract(cibles(J,LC)), supprimer(P2,LC,NLC),             % on actualise sa liste de cibles.
+                    retract(points(J,N)), assert(points(J,NewN)),           % On actualise les points
+                    retract(cibles(J,LC)), supprimer(P2,LC,NLC),            % On actualise sa liste de cibles.
                     assert(cibles(J,NLC)).
 
 donnePoints(P2):-   tour(J), points(J,NJ),
-                    tueur(J2,P2), J2\=J, points(J2,NJ2),                     % Si le joueur tué est le tueur à gage de l'autre.
+                    tueur(J2,P2), J2\=J, points(J2,NJ2),                    % Si le joueur tué est le tueur à gage de l'autre.
                     NewNJ is NJ + 3,                                        % Le joueur actuel gagne 3 points
                     NewNJ2 is NJ2 - 2,                                      % Son adversaire en perd 2 car il n'a plus son tueur à gage.
                     retract(points(J,_)), assert(points(J,NewNJ)),
